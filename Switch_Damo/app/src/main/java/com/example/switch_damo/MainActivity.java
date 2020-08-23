3b117b9f96d6ea265bdafbbe6422f28e98e4cc23
unknown <mahibulhassan842@gmail.com>
2020-08-23 21:46:48 +0600

    Switch make

---
+++ Switch_Damo/app/src/main/java/com/example/switch_damo/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.switch_damo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Switch switche;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        switche=(Switch)findViewById(R.id.switchid);
+
+        switche.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    Toast.makeText(MainActivity.this,"ON",Toast.LENGTH_SHORT).show();
+                }else
+                {
+                    Toast.makeText(MainActivity.this,"OFF",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
