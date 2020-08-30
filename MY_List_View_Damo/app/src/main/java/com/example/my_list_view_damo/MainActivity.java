15de64405f7d1d5a7836a8e02c3155617d4b799d
unknown <mahibulhassan842@gmail.com>
2020-08-30 21:40:18 +0600

    List View Damo

--- MY_List_View_Damo/app/src/main/java/com/example/my_list_view_damo/MainActivity.java
+++ MY_List_View_Damo/app/src/main/java/com/example/my_list_view_damo/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.my_list_view_damo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        list=(ListView)findViewById(R.id.listviewid);
+        String [] str=getResources().getStringArray(R.array.country_name);
+        ArrayAdapter<String>adapter= new ArrayAdapter<String>(MainActivity.this,R.layout.sample_view,R.id.textviewid,str);
+        list.setAdapter(adapter);
+
     }
 }
