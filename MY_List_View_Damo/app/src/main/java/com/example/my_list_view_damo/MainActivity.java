4c1fee763e776fe8072c50f71827404e4d05ae46
unknown <mahibulhassan842@gmail.com>
2020-09-01 20:25:12 +0600

    add lesener

--- MY_List_View_Damo/app/src/main/java/com/example/my_list_view_damo/MainActivity.java
+++ MY_List_View_Damo/app/src/main/java/com/example/my_list_view_damo/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.my_list_view_damo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private ListView list;
@@ -14,9 +17,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         list=(ListView)findViewById(R.id.listviewid);
-        String [] str=getResources().getStringArray(R.array.country_name);
+        final String [] str=getResources().getStringArray(R.array.country_name);
+
         ArrayAdapter<String>adapter= new ArrayAdapter<String>(MainActivity.this,R.layout.sample_view,R.id.textviewid,str);
         list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String Name = str[position];
+                Toast.makeText(MainActivity.this,Name+" "+position,Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
