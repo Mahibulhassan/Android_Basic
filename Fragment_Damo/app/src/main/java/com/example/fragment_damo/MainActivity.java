6c6fa31a9bccf3377a57c527c8f35bdd3f38ced5
unknown <mahibulhassan842@gmail.com>
2020-09-10 21:05:29 +0600

    Fragment demo

---
+++ Fragment_Damo/app/src/main/java/com/example/fragment_damo/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.fragment_damo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import static com.example.fragment_damo.R.id.listviewid;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    private ListView listview;
+    String [] Names;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listview=(ListView)findViewById(listviewid);
+        Names=getResources().getStringArray(R.array.friendsname);
+
+        ArrayAdapter<String>adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,Names);
+
+        listview.setAdapter(adapter);
+        listview.setOnItemClickListener(this);
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Fragment fragment;
+
+        if(position == 0){
+
+            fragment= new mahibulfragment();
+            FragmentManager fragmentmanager=getFragmentManager();
+            FragmentTransaction trasaction = fragmentmanager.beginTransaction();
+            trasaction.replace(R.id.fragmentid,fragment);
+            trasaction.commit();
+        }
+        if(position == 1){
+            fragment= new hasanfragment();
+            FragmentManager fragmentmanager=getFragmentManager();
+            FragmentTransaction trasaction = fragmentmanager.beginTransaction();
+            trasaction.replace(R.id.fragmentid,fragment);
+            trasaction.commit();
+        }
+
+    }
+}
